b69e73182fac57923cc8fbc11d46c35dc7a8035d
juliocesarlab <juliostdfac@gmail.com>
2022-03-16 13:32:29 -0300

    add slider

---
+++ scripts/slider.js
@@ -0,0 +1,24 @@
+let slideIndex = 1;
+showSlides();
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  console.log(slides)
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+} 
