4f2249b3eb6d7fe383d9056146439ba87d4a882f
juliocesarlab <juliostdfac@gmail.com>
2022-04-18 12:54:42 -0300

    add: message on dashboard home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,5 +3,7 @@
 @section('title', 'Dashboard')
 
 @section('content')
-
+  <section style="display: flex; width: 100%; justify-content: center; align-items: center; height: calc(100vh - 250px)">
+    <h1 style="font-size: 2.1rem; color: #b3b3ba" >Acesse uma das páginas do cabeçalho para <b>começar a edição.</b></h1>
+  </section>
 @endsection
